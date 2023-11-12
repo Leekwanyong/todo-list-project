181068733296b077b113518af45029818238a044
Leekwanyong <dlrkdid12@gmail.com>
2023-11-13 05:39:52 +0900

    다크모드 로컬스토리지 저장

--- src/darkmode/DarkModeContext.jsx
+++ src/darkmode/DarkModeContext.jsx
@@ -3,13 +3,15 @@ import { createContext, useEffect, useState } from "react";
 export const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const storedDarkMode = localStorage.getItem("darkMode") === "true";
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
 
   const darkModeBtn = () => {
     setDarkMode((mode) => !mode);
   };
 
   useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
     document.body.classList.toggle("dark", darkMode);
   }, [darkMode]);
   return (
